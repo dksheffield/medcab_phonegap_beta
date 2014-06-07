68fdb8945ea06cfc8124adb3b7823d41a1632888
David Sheffield <jeremiah.sheffield@gmail.com>
2014-06-07 12:06:43 +0000

    testing

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+function onBodyLoad() {
+    document.addEventListener("deviceready",onDeviceReady, false);
+}
+function onDeviceReady() {
+    $('#uuid').html(device.uuid);
+    setInterval(function(){ajaxCheck()}, 30000);
+}
+function ajaxCheck() {
+          var html = $('#debug_div').html();
+          html += '<hr />';
+          html += 'Checking via ajax<br />';
+          var prev_status = $('#local_storage_div').attr('data-network-status');
+          html += 'Prev status: ' + prev_status + '<br />';
+          var jqxhr = $.get( "http://www.google.com", function() {
+            html += 'new_status is online<br />';
+            var new_status = 'online';
+            toggleStatus(prev_status,new_status);
+            $('#debug_div').html(html);
+          })
+          .fail(function() {
+            html += 'new status is offline<br />'
+            var new_status = 'offline';
+            toggleStatus(prev_status,new_status)
+            $('#debug_div').html(html);
+          });
+          
+      }
+      function clearStorage() {
+          console.log('clearing storage');   
+      }
+      function hideAllDivs() {
+          var divs = ['offline_div','janrain_login_div','loading_div'];
+          for (x=0;x<divs.length;x++) {
+              $('#' + divs[x]).removeAttr('style');
+              $('#' + divs[x]).attr('style','display: none;');
+          }
+      }
+      function isSessionActive() {
+          //console.log(window.localStorage.getItem("sessionEndDate"));
+      }
+      function showDiv(divName) {
+          console.log(window.sessionStorage);
+          console.log(window.localStorage);
+          isSessionActive();
+          hideAllDivs();
+          $('#' + divName).removeAttr('style');          
+      }
+      function startApp() {
+          showDiv('loading_div');
+      }
+      function toggleStatus(prev_status,new_status) {
+        if (prev_status != new_status) {
+            $('#local_storage_div').attr('data-network-status',new_status);
+            if (new_status == 'online') {
+              $('#offline_div').removeAttr('style');
+              $('#offline_div').attr('style','display: none;');
+              $('#online_div').removeAttr('style');
+            } else {
+              $('#offline_div').removeAttr('style');
+              $('#online_div').removeAttr('style');
+              $('#online_div').attr('style','display: none;');
+            }
+        }
+      }
